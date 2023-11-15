b7b021fc6c8ee472ab47b2e5c256dfeb1dd06a4d
caio lucena <caio.lcolaco@gmail.com>
2023-11-15 19:01:19 -0300

    Change the select's boxes

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
-import { ExpandMore } from '@mui/icons-material'
+import { useState } from 'react'
+
 export default function Home() {
+  const [course, setCourse] = useState("ComputerScience")
+  const [quantity, setQuantity] = useState("5")
+
   return (
     <main className={styles.main}>
 
@@ -20,15 +25,27 @@ export default function Home() {
         </div>
 
         <div className={styles.menuButtons}>
-          <button>
-            Curso 
-            <ExpandMore color="white"/>
-          </button>
+            <select value={course} onChange={ e => setCourse(e.target.value)}>
+              <option value="ComputerScience">Ciências da Computação</option>
+              <option value="ComputerEngineering">Engenharia da Computação</option>
+              <option value="ADS">ADS</option>
+            </select>
 
-          <button>
-            Quantidade 
-            <ExpandMore color="white"/>
-          </button>
+            <select value={quantity} onChange={ e => setQuantity(e.target.value)}>
+              <option value="1">1 Cadeira</option>
+              <option value="2">2 Cadeiras</option>
+              <option value="3">3 Cadeiras</option>
+              <option value="4">4 Cadeiras</option>
+              <option value="5">5 Cadeiras</option>
+              <option value="6">6 Cadeiras</option>
+              <option value="7">7 Cadeiras</option>
+              <option value="8">8 Cadeiras</option>
+              <option value="9">9 Cadeiras</option>
+              <option value="10">10 Cadeiras</option>
+              <option value="11">11 Cadeiras</option>
+              <option value="12">12 Cadeiras</option>
+            </select>
+          
         </div>
       </div>
 
